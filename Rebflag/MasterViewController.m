8d0c8d5d0e77462766d5ff61458a121c39df5dfe
Niklas Holmgren <niklas@sutajio.se>
2017-03-04 22:54:29 +0100

    Section index for countries table.

--- Rebflag/MasterViewController.m
+++ Rebflag/MasterViewController.m
@@ -14,14 +14,14 @@
 
 @interface MasterViewController () <NHARequestDelegate>
 @property NHARequest *request;
-@property NSArray<NHACountry *> *countries;
+@property NSArray<NSArray<NHACountry *> *> *sections;
 @end
 
 @implementation MasterViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
     self.detailViewController = (DetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
     
     NSURL *URL = [NSURL URLWithString:@"https://restcountries.eu/rest/v2/all"];
@@ -55,8 +55,27 @@
 }
 
 - (void)request:(NHARequest *)request didCompleteWithResponse:(id)response {
-    self.countries = response;
+    self.sections = [self sectionsWithCountries:response];
     [self.tableView reloadData];
+}
+
+
+#pragma mark - Sections
+
+- (NSArray<NSArray<NHACountry *> *> *)sectionsWithCountries:(NSArray<NHACountry *> *)countries {
+    UILocalizedIndexedCollation *collation = [UILocalizedIndexedCollation currentCollation];
+    NSMutableArray<NSMutableArray<NHACountry *> *> *sections = [[NSMutableArray alloc] init];
+    
+    for (int i = 0; i < collation.sectionTitles.count; i++) {
+        [sections addObject:[[NSMutableArray alloc] init]];
+    }
+    
+    [[collation sortedArrayFromArray:countries collationStringSelector:@selector(name)] enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        NSInteger section = [collation sectionForObject:obj collationStringSelector:@selector(name)];
+        [sections[section] addObject:obj];
+    }];
+    
+    return sections;
 }
 
 
@@ -65,7 +84,7 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if ([[segue identifier] isEqualToString:@"showDetail"]) {
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        NHACountry *country = self.countries[indexPath.row];
+        NHACountry *country = self.sections[indexPath.section][indexPath.row];
         DetailViewController *controller = (DetailViewController *)[[segue destinationViewController] topViewController];
         [controller setCountry:country];
         controller.navigationItem.leftBarButtonItem = self.splitViewController.displayModeButtonItem;
@@ -77,19 +96,34 @@
 #pragma mark - Table View
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+    return self.sections.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.countries.count;
+    return self.sections[section].count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
 
-    NHACountry *country = self.countries[indexPath.row];
+    NHACountry *country = self.sections[indexPath.section][indexPath.row];
     cell.textLabel.text = country.name;
     return cell;
+}
+
+
+#pragma mark - Section headers & Index
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    return [UILocalizedIndexedCollation currentCollation].sectionTitles[section];
+}
+
+- (NSArray<NSString *> *)sectionIndexTitlesForTableView:(UITableView *)tableView {
+    return [UILocalizedIndexedCollation currentCollation].sectionIndexTitles;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index {
+    return [[UILocalizedIndexedCollation currentCollation] sectionForSectionIndexTitleAtIndex:index];
 }
 
 
