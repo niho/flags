d352dc06656fcb39c60b1247f6d67cdf88ffa739
Niklas Holmgren <niklas@sutajio.se>
2017-03-05 16:09:53 +0100

    Notify NHARequestDelegate methods on main queue.

--- Rebflag/NHARequest.m
+++ Rebflag/NHARequest.m
@@ -34,17 +34,17 @@ const int NHARequestDeserializationError = -1;
 - (void)fetch {
     [[self.session dataTaskWithRequest:self.request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         if (error) {
-            if (self.delegate && [self.delegate respondsToSelector:@selector(request:didFailWithError:)]) {
+            [self notifyDelegateOnMainQueue:^{
                 [self.delegate request:self didFailWithError:error];
-            }
+            }];
         } else {
             @try {
                 id object = [self deserialize:data];
                 if (object) {
                     id response = [self.decoder decode:object];
-                    if (self.delegate && [self.delegate respondsToSelector:@selector(request:didCompleteWithResponse:)]) {
+                    [self notifyDelegateOnMainQueue:^{
                         [self.delegate request:self didCompleteWithResponse:response];
-                    }
+                    }];
                 }
             } @catch (NSException *e) {
                 [self handleException:e];
@@ -57,7 +57,9 @@ const int NHARequestDeserializationError = -1;
     NSError *error;
     id object = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
     if (object == nil || error != nil) {
-        [self.delegate request:self didFailWithError:error];
+        [self notifyDelegateOnMainQueue:^{
+            [self.delegate request:self didFailWithError:error];
+        }];
     }
     return object;
 }
@@ -70,8 +72,14 @@ const int NHARequestDeserializationError = -1;
     NSError *error = [NSError errorWithDomain:NHARequestErrorDomain
                                          code:NHARequestDeserializationError
                                      userInfo:userInfo];
-    if (self.delegate && [self.delegate respondsToSelector:@selector(request:didFailWithError:)]) {
+    [self notifyDelegateOnMainQueue:^{
         [self.delegate request:self didFailWithError:error];
+    }];
+}
+
+- (void)notifyDelegateOnMainQueue:(void (^)())block {
+    if (self.delegate) {
+        dispatch_async(dispatch_get_main_queue(), block);
     }
 }
 
